a7d889a039493c36cff5d2c28530066668629936
up2app <dark-mhmd@hotmail.com>
2021-03-08 23:06:22 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace TxtFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtTimeInMin = new System.Windows.Forms.TextBox();
+            this.txtDesc = new System.Windows.Forms.TextBox();
+            this.TimeInMin = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.lblViewr = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(15, 86);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Recipe Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtName.Location = new System.Drawing.Point(197, 86);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(132, 22);
+            this.txtName.TabIndex = 3;
+            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
+            // 
+            // txtTimeInMin
+            // 
+            this.txtTimeInMin.Location = new System.Drawing.Point(197, 134);
+            this.txtTimeInMin.Margin = new System.Windows.Forms.Padding(4);
+            this.txtTimeInMin.Name = "txtTimeInMin";
+            this.txtTimeInMin.Size = new System.Drawing.Size(132, 22);
+            this.txtTimeInMin.TabIndex = 4;
+            // 
+            // txtDesc
+            // 
+            this.txtDesc.Location = new System.Drawing.Point(224, 184);
+            this.txtDesc.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDesc.Name = "txtDesc";
+            this.txtDesc.Size = new System.Drawing.Size(181, 22);
+            this.txtDesc.TabIndex = 5;
+            // 
+            // TimeInMin
+            // 
+            this.TimeInMin.AutoSize = true;
+            this.TimeInMin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.TimeInMin.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.TimeInMin.Location = new System.Drawing.Point(16, 134);
+            this.TimeInMin.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.TimeInMin.Name = "TimeInMin";
+            this.TimeInMin.Size = new System.Drawing.Size(113, 25);
+            this.TimeInMin.TabIndex = 6;
+            this.TimeInMin.Text = "Time in Min";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(15, 180);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(181, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Recipe Descreption";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(21, 318);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(133, 38);
+            this.btnAdd.TabIndex = 8;
+            this.btnAdd.Text = "Add to File";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(15, 234);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 25);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Recipe Code";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(197, 234);
+            this.txtCode.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(132, 22);
+            this.txtCode.TabIndex = 10;
+            // 
+            // lblViewr
+            // 
+            this.lblViewr.AutoSize = true;
+            this.lblViewr.Location = new System.Drawing.Point(100, 400);
+            this.lblViewr.Margin = new System.Windows.Forms.Padding(15, 0, 4, 0);
+            this.lblViewr.Name = "lblViewr";
+            this.lblViewr.Size = new System.Drawing.Size(0, 17);
+            this.lblViewr.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(186, 318);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 38);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Delete File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(347, 318);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 38);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Create File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(347, 379);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 38);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "View File";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(542, 556);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblViewr);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TimeInMin);
+            this.Controls.Add(this.txtDesc);
+            this.Controls.Add(this.txtTimeInMin);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtTimeInMin;
+        private System.Windows.Forms.TextBox txtDesc;
+        private System.Windows.Forms.Label TimeInMin;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.Label lblViewr;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TxtFile
+{
+    public partial class Form1 : Form
+    {
+        string inputfile = @"../../file1.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            int flag = 0;
+            foreach (var line in File.ReadAllLines(inputfile))//עוברים על כל שורה 
+            {
+                string[] inputs = line.Split(',');//מחלקים לפי הפסיק
+                if(inputs[3]==txtCode.Text)
+                {
+                    MessageBox.Show("Recipe already in");
+                    flag = 1;
+                }
+            }
+
+            if (txtCode.Text==""||txtDesc.Text==""||txtName.Text==""||txtTimeInMin.Text=="")
+            {
+                MessageBox.Show("Fill all inputs");
+                flag = 1;
+            }
+           if(flag==0)
+            {
+                //מוספים את פרטי המוצר בנוסף פסיק ביניהם
+                File.AppendAllText(inputfile, txtName.Text + "," + txtTimeInMin.Text + "," + txtDesc.Text + "," + txtCode.Text + "\n");
+                //משתנה שמכיל הטקסת שבקובץ
+                String input = File.ReadAllText(inputfile);
+                //הצגה
+                lblViewr.Text = input;
+                MessageBox.Show("Recipe added successfully");
+            }
+        }
+
+        private void btnbuy_Click(object sender, EventArgs e)
+        {
+             File.WriteAllText(inputfile, "");//איפוס file
+            String input = File.ReadAllText(inputfile);  //משתנה שמכיל הטקסת שבקובץ
+            lblViewr.Text = input;//הצגה
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(inputfile))
+            {
+                File.Delete(inputfile);
+                MessageBox.Show("File has been deleted");
+            }
+            else
+                MessageBox.Show("File not found");
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FileStream fs = null;
+            if (!File.Exists(inputfile))
+            {
+                using (fs = File.Create(inputfile))
+                {
+
+                }
+                MessageBox.Show("File has been Created");
+            }
+            else
+                MessageBox.Show("File already exists");
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(inputfile))
+            {
+                String input = File.ReadAllText(inputfile);
+                lblViewr.Text = input;
+            }
+            else
+                MessageBox.Show("File not exists");
+
+        }
+    }
+}
